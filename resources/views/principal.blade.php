0cdff4cf82f99c10d50842a71a036baeedd08054
salvadorc94 <secg.1994@gmail.com>
2020-12-07 22:13:13 -0600

    Integrated core UI template, modified it to meet our needs on student dashboard, left the admin dashboard for the future

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Centro de Servicio Social | UCA">
+    <meta name="author" content="">
+    <meta name="keyword" content="Sistema de inscripción de proyectos CSS UCA">
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <title>Centro de Servicio Social | UCA</title>
+    <!-- Icons -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/simple-line-icons.min.css" rel="stylesheet">
+    <!-- Main styles for this application -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
+    <header class="app-header navbar">
+        <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    <img src="img/UCA.6b065e25.png" class="img-avatar" alt="admin@bootstrapmaster.com">
+                    <span class="d-md-down-none"> 00117716 </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <div class="dropdown-header text-center">
+                        <strong>Cuenta</strong>
+                    </div>
+                    <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Perfil</a>
+                    <a class="dropdown-item" href="#"><i class="fa fa-lock"></i> Cerrar sesión</a>
+                </div>
+            </li>
+        </ul>
+    </header>
+
+    <div class="app-body">
+        <div class="sidebar">
+            <nav class="sidebar-nav">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="main.html"><i class="icon-home"></i> Inicio </a>
+                    </li>
+                    <li class="nav-title">
+                        Menú
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="icon-folder"></i> Proyectos Disponibles </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="icon-check"></i> Proyectos Aplicados </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="icon-calendar"></i> Historial de proyectos </a>
+                    </li>
+                </ul>
+            </nav>
+            <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+        </div>
+
+        <!-- Contenido Principal -->
+        @yield('edashboard')
+        <!-- /Fin del contenido principal -->
+    </div>
+
+    
+
+    <footer class="app-footer">
+        <span><a href="http://www.uca.edu.sv/servicio-social/">Centro de Servicio Social | UCA</a> &copy; 2017</span>
+        <span class="ml-auto">Desarrollado por <a href="#">Dos personas que quieren sus horas xD</a></span>
+    </footer>
+
+    <!-- Bootstrap and necessary plugins -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/pace.min.js"></script>
+    <!-- Plugins and scripts required by all views -->
+    <script src="js/Chart.min.js"></script>
+    <!-- GenesisUI main scripts -->
+    <script src="js/template.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home/edashboard');
+});
+
+Route::get('/admin', function () {
+    return view('admin/adashboard');
+});
