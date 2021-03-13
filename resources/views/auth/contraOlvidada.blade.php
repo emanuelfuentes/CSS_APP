5551637747d4c941e72c593f5bae511058c615b3
NitroSkate <00159817@uca.edu.sv>
2021-03-12 19:29:32 -0600

    CSS contrasenia olvidada

--- resources/views/auth/contraOlvidada.blade.php
+++ resources/views/auth/contraOlvidada.blade.php
@@ -4,23 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet'>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="./olvidec.css" rel="stylesheet">
+    <script src="js/plantilla.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <title>Document</title>
 </head>
-<body>
-    <h2>Cambiar contra</h2>
-    <h2>{{$user->correo}}</h2>
+<body class="cuerpo">
+    <div class="container">
+        <div class="card-group">
+            <h2>Cambiar contra</h2>
+            <h2>{{$user->correo}}</h2>
 
-    <form action="{{ url('/cambiar_contra_olvidada/'.$user->correo) }}" method="post">
-    {{ csrf_field() }}
-        Contraseña: <br>
-        <input type="password" name="password" id="password">
+            <form action="{{ url('/cambiar_contra_olvidada/'.$user->correo) }}" method="post">
+            {{ csrf_field() }}
+                Contraseña: <br>
+                <input type="password" name="password" id="password">
 
-        <br><br>
-        Confirmar contraseña: <br>
-        <input type="password" name="password_confirm" id="password_confirm">
+                <br><br>
+                Confirmar contraseña: <br>
+                <input type="password" name="password_confirm" id="password_confirm">
 
-        <button type="submit">Establecer contraseña</button>
+                <button type="submit">Establecer contraseña</button>
 
-    </form>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
--- resources/views/auth/verificarCuenta.blade.php
+++ resources/views/auth/verificarCuenta.blade.php
@@ -1,26 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet'>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="./olvidec.css" rel="stylesheet">
+    <script src="js/plantilla.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <title>Document</title>
 </head>
-<body>
-    <h2>Cambiar contra</h2>
-    <h2>{{$user->correo}}</h2>
+<body class="cuerpo">
+    <div class="container">
+        <div class="card-group">
+            <div class="card">
+                <div class="card-body" id="cuerpoc">
+                    <!--<img class="icon" src="img/lock.png" width="125" height="150">-->
+                    <h2 id="titulo">Actualice su contraseña</h2>
+                    <!--<h2 id="usuario">{{$user->correo}}</h2>-->
 
-    <form action="{{ url('/cambiar_contra/'.$user->correo) }}" method="post">
-    {{ csrf_field() }}
-        Contraseña: <br>
-        <input type="password" name="password" id="password">
+                    <form id="form" action="{{ url('/cambiar_contra/'.$user->correo) }}" method="post">
+                    {{ csrf_field() }}
+                        Usuario: <br>
+                        <input type="text" name="password" id="password" value="{{$user->correo}}" readonly>
+                        <br>
+                        Contraseña: <br>
+                        <input type="password" name="password" id="password">
 
-        <br><br>
-        Confirmar contraseña: <br>
-        <input type="password" name="password_confirm" id="password_confirm">
-
-        <button type="submit">Establecer contraseña</button>
-
-    </form>
+                        <br>
+                        Confirmar contraseña: <br>
+                        <input type="password" name="password_confirm" id="password_confirm">
+                        <br>
+                        <button type="submit" id="submit">Establecer contraseña</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+    <style>
+    <?php include "css/olvidec.css" ?>
+    </style>
+    <style>
+    <?php include "css/bootstrap.min.css" ?>
+    </style>
 </html>
