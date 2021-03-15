3e87af7885a406f8efcb5623d6e8e4992eb42244
The5econd <00017917@uca.edu.sv>
2021-03-14 20:39:56 -0600

    se crea usuario con carrera AL FIN PACHECO

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,6 +16,8 @@ return [
     'failedPass' => 'Correo o contraseña incorrectos',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'correo_no_verificado' => 'Su cuenta no está verificada. ¿Ya revisó su correo?',
-    'correo_inexistente' => 'No se pudo encontrar el correo ingresado en el registro.'
+    'correo_inexistente' => 'No se pudo encontrar el correo ingresado en el registro.',
+    'aun_no_verificado' => 'Correo ya existente pero no se encuentra verificado. Por favor revise su correo.',
+    'ya_verificado' => 'Este correo ya existe en el registro.'
 
 ];
