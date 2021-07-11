b9ef4363caf577cff9f33a5c1e36cc70675bbaf3
Joshua <00159817@uca.edu.sv>
2021-07-10 18:03:36 -0600

    Tocando Contraseña

--- resources/views/auth/contraOlvidada.blade.php
+++ resources/views/auth/contraOlvidada.blade.php
@@ -16,14 +16,14 @@
     <script src="js/bootstrap.min.js"></script>-->
     
     <title>Document</title>
-</head>
+</head><!--
 <body class="cuerpo">
     <div class="container">
         <div class="card-group">
             <div class="card">
                 <div class="card-body" id="cuerpoc">
                     <h2 id="titulo">Cambiar contraseña</h2>
-                    <!--<h2>{{$user->correo}}</h2>-->
+                    <!--<h2>{{$user->correo}}</h2>
 
                     <form id="form" action="{{ url('/cambiar_contra_olvidada/'.$user->correo) }}" method="post">
                     {{ csrf_field() }}
@@ -52,5 +52,38 @@
             </div>
         </div>
     </div>
+</body> -->
+
+<body>
+    <div class="container">
+        <div class="card-body" id="cuerpoc">
+            <h2 id="titulo">Cambiar contraseña</h2>
+
+            <form id="form" action="{{ url('/cambiar_contra_olvidada/'.$user->correo) }}" method="post">
+            {{ csrf_field() }}
+                <label for="user" class="label-form">Usuario</label>
+                <input class="mb-2" type="text" name="user" id="user" value="{{$user->correo}}" readonly>
+
+                <label for="password" class="label-form">Contraseña</label>
+                <input type="password" name="contraseña" id="contraseña">
+                @if($errors->first('contraseña'))
+                    {!!$errors->first('contraseña','<span style="color: red;">:message</span>')!!}
+                @else
+                    <span style="visibility: hidden;">.</span>
+                @endif
+
+                <label for="password_confirm" class="label-form">Confirmar contraseña</label>
+                <input type="password" name="confirmar" id="confirmar">
+                @if($errors->first('confirmar'))
+                    {!!$errors->first('confirmar','<span style="color: red;margin-bottom:0.5em">:message</span>')!!}
+                @else
+                    <span style="visibility: hidden; margin-bottom:0.5em">.</span>
+                @endif
+                <button type="submit" id="submit">Establecer contraseña</button>
+
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
