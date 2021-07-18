05ff0d4b0fd8c8f684cc3b539ec78cd818cace7f
Joshua <00159817@uca.edu.sv>
2021-07-17 22:40:33 -0600

    Cambios en UI

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -8,18 +8,17 @@
 </head>
 <main>
   <div class="container">
-    <span style="display: flex; flex-direction: column; justify-content: center; align-items: center">
-        <img src="/img/UCA.6b065e25.png" alt="uca" style="width: 164px; height: 220px;">
-        <p style="font-family: 'Abel'; font-size: 10px; color: white">Centro de Servicio Social</p>
+    <span>
+        <img src="/img/UCA.6b065e25.png" alt="uca" style="width: 200px; height: 300px;">
+        <p id="css">Centro de Servicio Social</p>
     </span>
-    <div style="display: flex; flex-direction: column; justify-content: center; align-items: center">
-        <h1>Oops, parece que algo salió mal.</h1>
-        <p>La página que está buscando no existe o no está disponible temporalmente.</p>
-        <p>Puede usar este botón para regresar.</p>
-        <div class="d-flex justify-content-center">
-            <a href="{{url('/')}}"> regresar</a>
-            
-        </div>
+    <h1 id="title">Oops!</h1>
+    <h2 id="subtitle">parece que algo salió mal. </h2>
+    <p id="texto">La página que está buscando no existe o no está disponible temporalmente.</p>
+    <p id="texto">Puede usar este botón para regresar.</p>
+    <div class="d-flex justify-content-center">
+        <a href="{{url('/')}}"> regresar</a>
+        
     </div>
     
 </div>
