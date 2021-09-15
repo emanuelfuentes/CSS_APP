3f70c2daa695e571318107a57d23b13c3d810979
MaPache07 <00170517@uca.edu.sv>
2021-09-15 12:13:17 -0600

    General: se ha corregido un error

--- app/Http/Controllers/ProyectosxCarreraController.php
+++ app/Http/Controllers/ProyectosxCarreraController.php
@@ -63,8 +63,10 @@ class ProyectosxCarreraController extends Controller
         ->where('proyectoxcarrera.limite_sup', '>=', $user->idPerfil)
         ->where('proyectoxcarrera.idCarrera', '=', $user->idCarrera)
         ->where('proyecto.fecha_inicio', '>=', date('Y-m-d'))
-        ->whereRaw('proyectoxestudiante.idUser is null')
+        ->whereRaw('(proyectoxestudiante.idUser !=' . $user->idUser . ' OR proyectoxestudiante.idUser IS NULL)')
+        ->whereRaw('proyecto.idProyecto NOT IN (SELECT p.idProyecto FROM proyecto p, proyectoxestudiante pe WHERE p.idProyecto = pe.idProyecto AND pe.idUser = ' . $user->idUser . ')')
         ->whereRaw('proyecto.cupos_act < proyecto.cupos')
+        ->groupBy('proyecto.idProyecto', 'proyecto.nombre', 'proyecto.descripcion', 'proyecto.estado', 'proyecto.tipo_horas', 'proyecto.cupos_act', 'proyecto.cupos', 'proyecto.horario', 'proyecto.encargado','proyecto.fecha_inicio','proyecto.fecha_fin')
         ->orderBy('proyecto.idProyecto', 'desc')->paginate(10);
         
         return [
